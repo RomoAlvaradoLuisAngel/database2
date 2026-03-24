a4d0794a752b07a1bbb0347129c20191d72b336d
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-03-24 08:13:18 -0500

    controllers

---
+++ src/controllers/TareasController.py
@@ -0,0 +1,15 @@
+from src.models.TareaModel import TareaModel
+
+class TareaController:
+    def __init__(self):
+        self.model = TareaModel
+            
+    def obtener_lista(self, id_usuario):
+        return self.model.listar_por_usuario(id_usuario)
+    
+    def guardas_nueva(Self, id_usuario, titulo, desc, prio, clas):
+        if not titulo:
+            return False, "El titulo es obligatorio"
+        
+        self.model.crear(id_usuario, titulo, desc, prio, clas)
+        return True, "Tarea guardada"
---
+++ src/controllers/UserController.py
@@ -0,0 +1,17 @@
+from src.models.UserModel import UsarioModel
+from src.models.schemasModel import UsuarioSchema
+from pydantic import ValidationError
+
+class AuthController:
+    def __init__(self):
+        self.model = UsuarioModel()
+        
+    def resgistrar_usuario(self, nombre, email, password):
+        try:
+            #validar datos con el schema
+            nuevo_usuario=UsuarioSchema(nombre=nombre, email=email, password=password)
+            success = self.model.registrar(nuevo_usuario)
+            return success, "Usuario creado correctamente"
+        except ValidationError as e:
+            #retorna el primer error de validacion encotrado
+            return False, e.errors()[0]['msg']
