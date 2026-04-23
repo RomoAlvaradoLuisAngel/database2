4982e07cae09e6fb41b6660804a9571f628c79d0
RomoAlvaradoLuisAngel <23308060610320@cetis61.edu.mx>
2026-04-23 11:10:53 -0500

    guardado pequeño

--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -6,9 +6,6 @@ def LoginView(page: ft.Page, AuthController):
     email_input = ft.TextField(label="Correo Electronico", width = 350, border_radius=10)
     pass_input = ft.TextField(label="Constraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
     
-    def verificar(usuario, contrasena):
-        return usuario == USUARIO_VALIDO and contrasena == CONTRASEñA_VALIDA
-    
     def login_click(e):
         if not email_input.value or not pass_input.value:
             page.snack_bar = ft.Snack_bar = ft.SnackBar(ft.Text("Por favor, llene todos los campos"))
@@ -27,13 +24,6 @@ def LoginView(page: ft.Page, AuthController):
             page.show_dialog(ft.SnackBar(ft.Text("Inicio de sesion exitoso.")))
             page.go("/dashboard")
             return
-        
-        if verificar(email_input.value, pass_input.value):
-            page.go("/dashboard")
-        else:
-            page.snack_bar = ft.SnackBar(ft.Text("Datos incorrectos"))
-            page.snack_bar.open = True
-            page.update()
                     
     login_button = ft.ElevatedButton("Entrar", on_click=login_click, width=350, bgcolor="blue", color = "white")
     registrar_button = ft.ElevatedButton("Crear una nueva cuenta", on_click=lambda _: page.go("/registro"), width=350, bgcolor="green", color = "white")
