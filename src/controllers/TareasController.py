fa0c9b2a6b9834bc57cf26645c974b18014b703c
RomoAlvaradoLuisAngel <23308060610320cetis61.edu.mx>
2026-04-26 00:41:07 -0600

    Estoy cansado jefe

--- src/controllers/TareasController.py
+++ src/controllers/TareasController.py
@@ -5,20 +5,7 @@ class TareaController:
         self.model = TareaModel()
             
     def obtener_lista(self, id_usuario):
-        return [
-            {
-                "titulo": "Tarea de Prueba 1",
-                "descripcion": "Esta es una tarea simulada",
-                "prioridad": "Alta",
-                "estado": "Pendiente"
-            },
-            {
-                "titulo": "Tarea de Prueba 2",
-                "descripcion": "Flet funcionando sin base de datos",
-                "prioridad": "Baja",
-                "estado": "Completada"
-            }
-        ]
+        return self.model.listar_por_usuario(id_usuario)
     
     def guardar_nueva(self, id_usuario, titulo, desc, prio, clas):
         if not titulo:
--- src/views/LoginView.py
+++ src/views/LoginView.py
@@ -1,54 +1,28 @@
 import flet as ft
-USUARIO_VALIDO="admin"
-CONTRASEñA_VALIDA = "123456"
 
-def LoginView(page: ft.Page, AuthController):
+def LoginView(page: ft.Page, auth_controller):
     email_input = ft.TextField(label="Correo Electronico", width = 350, border_radius=10)
     pass_input = ft.TextField(label="Constraseña", password=True, can_reveal_password=True, width=350, border_radius=10)
     
     def login_click(e):
         if not email_input.value or not pass_input.value:
-            page.snack_bar = ft.Snack_bar = ft.SnackBar(ft.Text("Por favor, llene todos los campos"))
+            page.snack_bar = ft.SnackBar(ft.Text("Por favor, llene todos los campos"))
             page.snack_bar.open = True
             page.update()
             return
-
-        def mostrar_dashboard():
-            page.clean()  # limpia la pantalla
-
-            page.add(
-                ft.AppBar(
-                    title=ft.Text("Panel principal"),
-                    bgcolor=ft.Colors.BLUE_900,
-                    color=ft.Colors.WHITE
-                ),
-                ft.Column(
-                    [
-                        ft.Icon(ft.Icons.BOLT, color=ft.Colors.YELLOW, size=100),
-                        ft.Text("Bienvenido al sistema."),
-                        ft.Text("Has iniciado sesion correctamente.")
-                    ],
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                    expand=True
-                )
-            )
-
-            page.update()
-        
-        
         usuario = email_input.value
         contrasena = pass_input.value
         
-        if usuario == USUARIO_VALIDO and contrasena == CONTRASEñA_VALIDA:
-            page.user_data = {
-                "nombre": "Admin",
-                "id_usuario": 1
-            }
-            page.show_dialog(ft.SnackBar(ft.Text("Inicio de sesion exitoso.")))
-            page.go(mostrar_dashboard)
-            return
-                    
+        user, msg = auth_controller.login(usuario, contrasena)
+
+        if user:
+            page.user_data = user
+            page.go("/dashboard")
+        else:
+            page.snack_bar = ft.SnackBar(ft.Text(msg))
+            page.snack_bar.open = True
+            page.update()
+                        
     login_button = ft.ElevatedButton("Entrar", on_click=login_click, width=350, bgcolor="cyan", color = "black", icon=(ft.Icon(ft.Icons.MAIL, color=ft.Colors.WHITE, size=25)))
     registrar_button = ft.ElevatedButton("Crear una nueva cuenta", on_click=lambda _: page.go("/registro"), width=350, bgcolor="green", color = "black", icon=(ft.Icon(ft.Icons.PASSWORD, color=ft.Colors.WHITE, size=25)))
     
