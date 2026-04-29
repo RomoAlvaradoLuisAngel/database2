5dd87b96a0c53fa89dc3978a7d334b72b226fd4e
RomoAlvaradoLuisAngel <23308060610320cetis61.edu.mx>
2026-04-28 21:39:38 -0600

    haber

--- src/models/schemasModel.py
+++ src/models/schemasModel.py
@@ -3,9 +3,13 @@ from typing import Optional
 from datetime import date, time
 class UsuarioBaseSchema(BaseModel):
     email: EmailStr
-    passwrod: str=Field(min_length=8)   
+    password: str=Field(min_length=8)   
 class UsuarioSchema(UsuarioBaseSchema):
     nombre: str = Field(min_length=8, max_length=100) 
+    
+class UsuarioLogin(UsuarioBaseSchema):
+    pass
+
 class TareaSchema(BaseModel):
     titulo: str = Field(min_length=1, max_length=200)
     descripcion: Optional[str] = None
